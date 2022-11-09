7589d43650820458907bac1084f201fbe629327f
Giannis Kafousis <g.kafousis@gmail.com>
2022-11-09 17:35:10 +0200

    hello world

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// services are registered at the root module
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+
+  // import modules
   imports: [
     BrowserModule
   ],
+
+  // register components, directives or pipes
+  declarations: [
+    AppComponent
+  ],
+
+  // register services
   providers: [],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
