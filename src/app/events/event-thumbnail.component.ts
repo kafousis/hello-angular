1fe250b9294f077554aca8b3873269f9a1fa103e
Giannis Kafousis <g.kafousis@gmail.com>
2022-11-10 22:43:36 +0200

    ngFor events

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -3,24 +3,21 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
     selector: 'event-thumbnail',
     template: `
-    <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span>&nbsp;</span>
-            <span>{{event.location.city}}, {{event.location.country}}</span>
+        <div class="well hoverwell thumbnail">
+            <h2>{{event.name}}</h2>
+            <div>Date: {{event.date}}</div>
+            <div>Time: {{event.time}}</div>
+            <div>Price: \${{event.price}}</div>
+            <div>
+                <span>Location: {{event.location.address}}</span>
+                <span>&nbsp;</span>
+                <span>{{event.location.city}}, {{event.location.country}}</span>
+            </div>
+            <!-- 
+            <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
+            -->
         </div>
-        <button class="btn btn-primary" (click)="handleClickMe()">
-            Click me!
-        </button>
-    </div>
-    `,
-    styles: [
-        
-    ]
+    `
 })
 
   export class EventThumbnailComponent {
